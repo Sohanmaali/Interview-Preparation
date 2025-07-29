a0b9202e7623d342971fca7c8f5100373da99569
sohan maali <sohanmaali4@gmail.com>
2025-07-29 20:44:09 +0530

    js program

--- jsProgram.js
+++ jsProgram.js
@@ -54,3 +54,62 @@
 //   return u[1];
 // }
 // console.log(secondLargest([50, 10, 20, 20, 30]));
+
+// swap Number
+// let a = 3;
+// let b = 8;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a);
+// console.log(b);
+
+// let a = 3;
+// let b = 8;
+// a = a * b;
+// b = a / b;
+// a = a / b;
+// console.log(a);
+// console.log(b);
+
+// Reverse Number
+// let a = 894;
+// let rev = 0;
+// for (; a > 0; a = Math.floor(a / 10)) {
+//   let tem = a % 10;
+//   rev = tem + rev * 10;
+// }
+// console.log(rev);
+
+// Delete Duplicate
+// let arr = [4, 3, 5, 4,2, 3, 6, 6];
+// for (let i = 0; i < arr.length; i++) {
+//   let n = arr[i];
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (n === arr[j]) {
+//       for (let k = j; k < arr.length - 1; k++) {
+//         arr[k] = arr[k + 1];
+//       }
+//       arr.pop();
+//     }
+//   }
+// }
+// console.log(arr);
+
+// Find Max
+// let arr = [4, 3, 5, 40, 2, 3, 6, 6];
+// let max = 0;
+// for (let i = 0; i < arr.length; i++) if (arr[i] > max) max = arr[i];
+// console.log(max);
+
+// Find Min
+// let arr = [4, 3, 5, 40, 1, 3, 6, 6];
+// let min = arr[0];
+// for (let i = 0; i < arr.length; i++) if (arr[i] < min) min = arr[i];
+// console.log(min);
+
+// Find Element
+// let arr = [4, 3, 5, 40, 1, 3, 6, 6];
+// let n = 4;
+// let is = arr.find((i) => i == n);
+// console.log(is);
